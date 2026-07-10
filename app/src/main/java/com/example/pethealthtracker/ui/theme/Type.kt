92738c5726db295e49084371824ac84246e7c02f
Richelle Mendez <richellemendez35@gmail.com>
2026-07-10 22:11:56 +0800

    pet arrangement

--- app/src/main/java/com/example/pethealthtracker/ui/theme/Type.kt
+++ app/src/main/java/com/example/pethealthtracker/ui/theme/Type.kt
@@ -6,6 +6,9 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
+
+
+
 // Set of Material typography styles
 val Typography = Typography(
     bodyLarge = TextStyle(
